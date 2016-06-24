abdee4ee5326c9d20580933411654caf854bdb41
Dominik <dominikilshofen@users.noreply.github.com>
2016-06-25 00:37:38 +0200

    created interfaces

---
+++ src/main/java/de/tilosp/vokabeltrainer/GUI.java
@@ -0,0 +1,54 @@
+package de.tilosp.vokabeltrainer;
+
+import javax.swing.*;
+
+/**
+ * Created by Dominik on 22.06.2016.
+ */
+public class GUI {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel;
+    private JPanel createPanel;
+    private JPanel practicePanel;
+    private JPanel editPanel;
+    private JButton addWord;
+    private JLabel primaryLanguage;
+    private JLabel secondaryLanguage;
+    private JTextField inputprimaryWord;
+    private JTextField inputSeondaryWord;
+    private JComboBox chosenLanguageAdd;
+    private JTextField firstLanguageInput;
+    private JTextField secondLanguageInput;
+    private JButton addSubjectButton;
+    private JCheckBox swapDirektionCheckBox;
+    private JLabel seenWord;
+    private JLabel knownWord;
+    private JComboBox chosenLanguagePractice;
+    private JButton showButton;
+    private JButton knownButton;
+    private JButton unknownButton;
+    private JComboBox comboBox1;
+    private JTable wordTable;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("GUI");
+        frame.setContentPane(new GUI().panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+
+    private void createUIComponents() {
+        String[] headLine = {"numbre", "first language", "second language", "word level", "bearbeiten"};
+
+        Object[][] data = new Object[15/*anzahl der Vokabelpaare*/][5];
+
+        for(int x = 0; x < 15; x++){
+            data[x] = new Object[]{x,"tell", "sagen", x+1, 1};
+        }
+
+        wordTable = new JTable(data,headLine);
+    }
+
+}
